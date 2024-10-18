03070a9ecb62c7b86d25167bfd7d3e14a32865c2
NIKKOGNG <97797545+NIKKOGNG@users.noreply.github.com>
2024-10-18 15:24:51 +0300

    Add files via upload

---
+++ Module_1/Variables.py
@@ -0,0 +1,6 @@
+Quantity_completed_HW = 12
+Quantity_spent_hours = 1.5
+Course_name = "Python"
+Time_for_one_task = Quantity_spent_hours / Quantity_completed_HW
+print(f'Курс: {Course_name},всего задач: {Quantity_completed_HW},'
+      f'затрачено часов: {Quantity_spent_hours},среднее время выполнения {Time_for_one_task} часа.')
